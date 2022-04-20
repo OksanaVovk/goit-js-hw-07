c76a972d7007f1667e12a5edc7be16cb537d7dbb
OksanaVovk <oksvovk85@gmail.com>
2022-04-20 14:37:58 +0300

    перекинуто код в інший файл

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,44 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryEl = document.querySelector(".gallery");
+const galleryCards = createGalleryCard(galleryItems);
+galleryEl.innerHTML = galleryCards;
+
+function createGalleryCard(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+    return `
+  <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+    </a>
+    </div>`;
+  })
+    .join('');
+}
+
+galleryEl.addEventListener("click", onGalleryElClick);
+
+function onGalleryElClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const htmlInstance = basicLightbox.create(`
+    <img
+      class="gallery__image"
+      src="${event.target.dataset.source}"
+      data-source="${event.target.dataset.source}"
+      alt="${event.target.alt}"
+    />`
+    );
+  htmlInstance.show();
+ 
+};
--- js/gallery-items.js
+++ js/gallery-items.js
@@ -65,45 +65,6 @@ export const galleryItems = [
 ];
 
 
-const galleryEl = document.querySelector(".gallery");
-const galleryCards = createGalleryCard(galleryItems);
-galleryEl.innerHTML = galleryCards;
-
-function createGalleryCard(galleryItems) {
-  return galleryItems.map(({ preview, original, description }) => {
-    return `
-  <div class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-    </a>
-    </div>`;
-  })
-    .join('');
-}
-
-galleryEl.addEventListener("click", onGalleryElClick);
-
-function onGalleryElClick(event) {
-  event.preventDefault();
-  if (!event.target.classList.contains("gallery__image")) {
-    return;
-  }
-  const htmlInstance = basicLightbox.create(`
-    <img
-      class="gallery__image"
-      src="${event.target.dataset.source}"
-      data-source="${event.target.dataset.source}"
-      alt="${event.target.alt}"
-    />`
-    );
-  htmlInstance.show();
- 
-};
 
 
 
